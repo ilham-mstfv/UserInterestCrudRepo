55880f12f614e036d8f30758ef0e0a1d496bd75e
Ilham <im615143@gmail.com>
2025-03-24 15:03:40 +0400

    Edit UserTest class
    Make User properties static, edit createUserTest method, add more assert methods

--- src/test/java/com/example/userinterestcrudrepo/entities/UserTest.java
+++ src/test/java/com/example/userinterestcrudrepo/entities/UserTest.java
@@ -11,19 +11,35 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class UserTest {
 
+    static int userId = 1;
+    static String userPersonalData = "John Doe";
+    static int userAge = 20;
+    static List<Interest> userInterests = List.of(
+            new Interest(1, "soccer", "sport", "american football"),
+            new Interest(1, "football", "sport", "some kind of sport"),
+            new Interest(1, "handball", "sport", "some kind of sport")
+    );
+    static Country userCountry = Country.AZE;
+    static Lang userLang = Lang.RUS;
+    static BigDecimal userBalance = BigDecimal.valueOf(100.2);
+
+    
     @Test
     void createUserTest() {
         User user = new User(
-                1,
-                "John Doe", 20,
-                List.of(
-                        new Interest(1, "soccer", "sport", "american football")
-                ),
-                Country.AZE,
-                Lang.RUS,
-                BigDecimal.valueOf(100.2)
+                userId, userPersonalData, userAge,
+                userInterests,
+                userCountry, userLang, userBalance
         );
-        assertThat(user.getId()).isNotNull().isEqualTo(1);
-        assertThat(user.getPersonalData()).isNotNull().isEqualTo("John Doe");
+
+        assertThat(user.getId()).isNotNull().isEqualTo(userId);
+        assertThat(user.getPersonalData()).isNotNull().isEqualTo(userPersonalData);
+        assertThat(user.getAge()).isNotNull().isEqualTo(userAge);
+        assertThat(user.getInterests()).isNotNull().isEqualTo(userInterests);
+        assertThat(user.getCountry()).isEqualTo(userCountry);
+        assertThat(user.getLang()).isEqualTo(userLang);
+        assertThat(user.getBalance()).isEqualTo(userBalance);
     }
+
+
 }
