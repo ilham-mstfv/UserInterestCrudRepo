f0228290d83195c09a75bdf854c436f604abac89
Ilham <im615143@gmail.com>
2025-04-01 15:11:43 +0400

    Remove useless dependency

--- src/main/java/com/example/userinterestcrudrepo/controllers/AuthController.java
+++ src/main/java/com/example/userinterestcrudrepo/controllers/AuthController.java
@@ -7,7 +7,6 @@ import com.example.userinterestcrudrepo.models.AuthResponse;
 import com.example.userinterestcrudrepo.services.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
