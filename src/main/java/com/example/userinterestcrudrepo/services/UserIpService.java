c5237817837c79d9df70d62647b6f338e5f3c5ff
Ilham <im615143@gmail.com>
2025-04-13 20:39:52 +0400

    Edit filtering method in UserIpService

--- src/main/java/com/example/userinterestcrudrepo/services/UserIpService.java
+++ src/main/java/com/example/userinterestcrudrepo/services/UserIpService.java
@@ -39,14 +39,26 @@ public class UserIpService {
     }
 
     public List<UserIp> getUserIpListByFilter (UserIpFilterRequest filter) {
-        StringBuilder query = new StringBuilder("SELECT u FROM UserIp u WHERE ");
-        if (filter != null) {
-            if (filter.getZonedDateTime() != null) {
-                query.append(" u.epochSecond >= ")
-                        .append(filter.getZonedDateTime().getFrom().toEpochSecond());
-                query.append(" AND u.epochSecond <= ")
-                        .append(filter.getZonedDateTime().getTo().toEpochSecond());
-            }
+        StringBuilder query = new StringBuilder("SELECT u FROM UserIp u WHERE 1=1 ");
+
+        if (filter.getUsername() != null) {
+            query.append(" AND u.username LIKE '%")
+                    .append(filter.getUsername()).append("%'");
+        }
+        if (filter.getIp() != null) {
+            query.append(" AND u.ip = '").append(filter.getIp()).append("'");
+        }
+        if (filter.getEndpoint() != null) {
+            query.append(" AND u.endpoint = '").append(filter.getEndpoint()).append("'");
+        }
+        if (filter.getRequestMethod() != null) {
+            query.append(" AND u.requestMethod = '").append(filter.getRequestMethod()).append("'");
+        }
+        if (filter.getDateTime() != null) {
+            query.append(" AND u.epochSecond >= ")
+                    .append(filter.getDateTime().getFrom().toEpochSecond());
+            query.append(" AND u.epochSecond <= ")
+                    .append(filter.getDateTime().getTo().toEpochSecond());
         }
         return em.createQuery(query.toString(), UserIp.class)
                 .getResultList();
