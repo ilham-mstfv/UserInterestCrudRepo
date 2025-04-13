598121218a155e08c49cb18427e942d0a915dbfc
Ilham <im615143@gmail.com>
2025-04-13 19:54:54 +0400

    Remove deprecated method csrf().disable(), replace it with csrf(AbstractHttpConfigurer::disable)

--- src/main/java/com/example/userinterestcrudrepo/config/SecurityConfig.java
+++ src/main/java/com/example/userinterestcrudrepo/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,7 +39,7 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
+        return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/users/auth/log").permitAll()
                     .anyRequest().authenticated()
