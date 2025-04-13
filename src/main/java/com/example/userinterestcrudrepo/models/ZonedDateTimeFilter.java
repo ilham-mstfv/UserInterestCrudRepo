94b6fd9721a547fed0ee657bd2d8c387ee90aea2
Ilham <im615143@gmail.com>
2025-04-13 19:30:11 +0400

    Add new ZonedDateTimeFilter class

---
+++ src/main/java/com/example/userinterestcrudrepo/models/ZonedDateTimeFilter.java
@@ -0,0 +1,17 @@
+package com.example.userinterestcrudrepo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ZonedDateTimeFilter {
+
+    private ZonedDateTime from;
+
+    private ZonedDateTime to;
+}
